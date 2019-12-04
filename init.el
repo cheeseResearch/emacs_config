f58301f4f2c6591032aa5750636dee82d68f45f2
cheeseResearch <50796713+cheeseResearch@users.noreply.github.com>
2019-12-04 11:17:23 +0000

    Update init.el
    Electric pair mode saves heaps of time from not faffing with brackets

--- init.el
+++ init.el
@@ -44,6 +44,10 @@
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
 (helm-mode 1)
+
+;; Auto close brackets to save time
+(electric-pair-mode 1)
+
 ;; PYTHON CONFIGURATION
 ;; --------------------------------------
 
